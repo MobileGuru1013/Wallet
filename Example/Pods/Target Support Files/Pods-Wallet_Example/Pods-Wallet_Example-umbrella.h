5a7550aadd5fc97b5e3140c79fbbed327d6ff665
Mobile Guru <alexandrlesan@yahoo.com>
2019-05-04 17:52:59 -0800

    Wallet
    Wallet is a library to manage cards and passes.

---
+++ Example/Pods/Target Support Files/Pods-Wallet_Example/Pods-Wallet_Example-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Wallet_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Wallet_ExampleVersionString[];
+
